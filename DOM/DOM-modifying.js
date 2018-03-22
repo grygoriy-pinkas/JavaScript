755b0fc801b97f19829e14cf92ba988b6cf8a92a
Grisha Pinkas <gricha.p@gmail.com>
2018-03-22 21:08:20 +0100

    n2 to DOM-modifying

--- DOM/DOM-modifying.js
+++ DOM/DOM-modifying.js
@@ -23,18 +23,15 @@
 // </script>
 
 
-var result = parent.contains(child);
+if (document.documentElement.remove === undefined) { // (1)
 
+    Element.prototype.remove = function remove() {
+        let parent = this.parentNode;
 
-if (document.documentElement.remove() === undefined) { // (1)
-
-    Element.prototype.remove(elem) = function {
-        let parent = elem.parentNode;
-        let result = parent.contains(elem);
-        if (result) {
-            parent.removeChild(elem);
+        if (parent) {
+            parent.removeChild(this);
         }
-        return elem;
+        return this;
     }
 }
 
@@ -46,6 +43,7 @@ if (document.documentElement.remove() === undefined) { // (1)
 // <div>Элементы</div> */}
 
 // <script>
+//тут ти пропустив задачку. по ній є ще питання нижче
 //працює другий варіант, перший чомусь ні
 var elem = document.createElement('div');
 elem.innerHTML = '<b>Новый элемент</b>';
@@ -59,7 +57,6 @@ var body = document.body;
 
 // вставить elem после первого элемента
 insertAfter(elem, body.firstChild); // <--- должно работать
-
 // вставить elem за последним элементом
 insertAfter(elem, body.lastChild); // <--- должно работать
 // </script>
@@ -199,54 +196,38 @@ var data = {
         }
     }
 };
-Синтаксис:
+// Синтаксис:
 
 
+var container = document.getElementById('tree');
 
-    var container = document.getElementById('tree');
-var ul = document.createElement('ul');
-container.appendChild(ul);
-
-
-// var key = Object.keys(data);
-//пропадає вложеність
-// console.log(key);
-// console.log(typeof key);
 function tree(container, data) {
-    //console.log(container);
+
     var indent = ' ';
-    if (Object.keys(data).length != 0) {
-        //console.log(Object.keys(data));
-        var ul = document.createElement('ul');
-        container.appendChild(ul);
-    }
+    var ul = document.createElement('ul');
+    container.appendChild(ul);
+
     for (let value in data) {
 
         let li = document.createElement('li');
         li.textContent = indent + value;
         ul.appendChild(li);
-        console.log(data[value]);
 
-        if (typeof data[value] == 'object') {
+        if (Object.keys(data).length != 0) {
+            container = li;
             tree(container, data[value]);
         }
     }
 }
 
-// var ul = document.createElement('ul');
-// container.appendChild(ul);
-
 tree(container, data);
-
-// for (let value in data) {
-//     console.log(value);
-//     if (typeof data[value] == 'object') {
-//         for (let key in data[value]) {
-//             console.log(key);
-//         }
-//     }
-
-// }
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//я це зробив. проблема в тому що не розумію де виникають пусті ul. 
+//єдине що приходить на думку - це ті що на початку функції. АЛЕ ВОНИ НА ПОЧАТКУ, тут треба твоєї допомоги
+//також підскажи мені в загальних словах як має виглядати інший варіант з інер.
+//по indent, то я запамятав цей трюк коли дивився уроки по laravel, там таке використовували)
+// P.S. біда заставила користуватися отладчиком)))
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // Выберите один из двух способов решения этой задачи:
 
 // Создать строку, а затем присвоить через container.innerHTML.
@@ -254,3 +235,87 @@ tree(container, data);
 // Если получится – сделайте оба.
 
 // P.S. Желательно, чтобы в дереве не было лишних элементов, в частности – пустых <ul></ul> на нижнем уровне.
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+
+Дерево
+// важность: 5
+// Есть дерево, организованное в виде вложенных списков <ul>/<li>.
+
+// Напишите код, который добавит каждому элементу списка <li> количество вложенных в него элементов. Узлы 
+// нижнего уровня, без детей – пропускайте.
+
+//подібне я вже робив. тут тільки підправити довелося.
+var elem = document.body.getElementsByTagName('li');
+
+for (let index = 0; index < elem.length; index++) {
+    var count = elem[index].getElementsByTagName('li');
+    if (count.length != 0) {
+        elem[index].firstChild.textContent = elem[index].firstChild.textContent + '[' + count.length + ']';
+    }
+    console.log(count);
+}
+
+//варіант з розвязку(в них завжди коротше(()))
+var lis = document.getElementsByTagName('li');
+
+for (i = 0; i < lis.length; i++) {
+    // получить количество детей
+    var childCount = lis[i].getElementsByTagName('li').length;
+    if (!childCount) continue;
+
+    // добавить кол-во детей к текстовому узлу
+    lis[i].firstChild.data += ' [' + childCount + ']';
+
+}
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+Создать календарь в виде таблицы
+// важность: 4
+// Напишите функцию, которая умеет генерировать календарь для заданной пары(месяц, год).
+
+// Календарь должен быть таблицей, где каждый день– это TD.У таблицы должен быть заголовок с названиями дней
+// недели, каждый день– TH.
+
+// Синтаксис: createCalendar(id, year, month).
+
+// Такой вызов должен генерировать текст для календаря месяца month в году year, а затем помещать его внутрь
+// элемента с указанным id.
+
+// Например: createCalendar("cal", 2012, 9) сгенерирует в < div id = „cal“ > < /div> следующий календарь:
+
+// P.S.Достаточно сгенерировать календарь, кликабельным его делать не нужно.
+
+//<div id=„cal“></div>
+
+//тут ще не дороблено. користувався підказками і овсім трішечки заглянув в код на початок
+//Uncaught TypeError: Cannot set property 'innerHTML' of null - тут така помилочка. хоча в коді так само 
+//зроблено
+
+
+function createCalendar(id, year, month) {
+    var table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
+
+    var element = document.getElementById(id);
+    let date = new Date(year, month - 1);
+    let day = date.getDay();
+
+    console.log(day);
+    for (let i = 0; i < day; i++) {
+        table += '<td></td>';
+    }
+
+    table += '</tr></table>';
+
+    // только одно присваивание innerHTML
+    element.innerHTML = table;
+}
+
+createCalendar("cal", 2012, 9);
+
+-- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+Часики с использованием "setInterval"
+важность: 4
