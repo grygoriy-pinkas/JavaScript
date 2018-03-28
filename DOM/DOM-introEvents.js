3febd82e89fe6009149ec04c74e0f7783936be82
Grisha Pinkas <gricha.p@gmail.com>
2018-03-28 16:19:44 +0200

    n2 to DOM-introEvents

--- DOM/DOM-introEvents.js
+++ DOM/DOM-introEvents.js
@@ -18,11 +18,15 @@
 var text = document.getElementById('text');
 var button = document.getElementById('hider');
 
+//button.addEventListener('click', () => {
+//      text.innerHTML = ''
+//   });
+
 button.addEventListener('click', () => {
-        text.innerHTML = ''
-    })
-    //     </script>
-    // </body>
+    text.style.display = 'none';
+});
+//     </script>
+// </body>
 
 // </html>
 
@@ -45,11 +49,30 @@ button.addEventListener('click', () => {
 
 
 //     <script>
+//old variant
 var button = document.getElementById('hider');
 
 button.addEventListener('click', () => {
-        button.style.display = 'none';
+    button.style.display = 'none';
+})
+
+//first new variant
+function hider(elem) {
+    var element = document.getElementById(elem);
+    //спочатку тут використовував стрілочну функцію. не міг вивести thisю а потім ЗГАДАВ що вони не мають
+    //контексту свого)
+    element.addEventListener('click', function() {
+        this.style.display = 'none';
     })
+}
+hider('hider');
+
+//second new variant
+var button = document.getElementById('hider');
+
+button.onclick = function() {
+        this.style.display = 'none';
+    }
     //     </script>
     // </body>
 
@@ -148,6 +171,164 @@ console.log(typeof button);
 });
 //     </script>
 
+// </body>
+
+// </html>
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+
+Карусель
+// важность: 4
+// Напишите «Карусель» – ленту изображений, которую можно листать влево-вправо нажатием на стрелочки.
+
+
+// В дальнейшем к ней можно легко добавить анимацию, динамическую подгрузку и другие возможности.
+
+// P.S. В этой задаче разработка HTML/CSS-структуры сос тавляет 90% решения.
+
+// <!DOCTYPE html>
+
+// <head>
+//     <meta charset="utf-8">
+
+//     <style>
+//         body {
+//             padding: 10px;
+//         }
+
+//         .carousel {
+//             position: relative;
+//             width: 398px;
+//             padding: 10px 40px;
+//             border: 1px solid #CCC;
+//             border-radius: 15px;
+//             background: #eee;
+//         }
+
+//         .carousel img {
+//             width: 130px;
+//             height: 130px;
+//             /* по умолчанию inline, в ряде браузеров это даст лишнее пространство вокруг элементов */
+//             display: block;
+//         }
+
+//         .arrow {
+//             position: absolute;
+//             top: 60px;
+//             padding: 0;
+//             background: #ddd;
+//             border-radius: 15px;
+//             border: 1px solid gray;
+//             font-size: 24px;
+//             line-height: 24px;
+//             color: #444;
+//             display: block;
+//         }
+
+//         .arrow:focus {
+//             outline: none;
+//         }
+
+//         .arrow:hover {
+//             background: #ccc;
+//             cursor: pointer;
+//         }
+
+//         .prev {
+//             left: 7px;
+//         }
+
+//         .next {
+//             right: 7px;
+//         }
+
+//         .gallery {
+//             width: 390px;
+//             overflow: hidden;
+//         }
+
+//         .gallery ul {
+//             height: 130px;
+//             width: 9999px;
+//             margin: 0;
+//             padding: 0;
+//             list-style: none;
+//             transition: margin-left 250ms;
+//             /* remove white-space between inline-block'ed li */
+//             /* http://davidwalsh.name/remove-whitespace-inline-block */
+//             font-size: 0;
+//         }
+
+//         .gallery li {
+//             display: inline-block;
+//         }
+//     </style>
+// </head>
+
+// <body>
+
+//     <div id="carousel" class="carousel">
+//         <button class="arrow prev">⇦</button>
+//         <div class="gallery">
+//             <ul class="images">
+//                 <li><img src="https://js.cx/carousel/1.png"></li>
+//                 <li><img src="https://js.cx/carousel/2.png"></li>
+//                 <li><img src="https://js.cx/carousel/3.png"></li>
+//                 <li><img src="https://js.cx/carousel/4.png"></li>
+//                 <li><img src="https://js.cx/carousel/5.png"></li>
+//                 <li><img src="https://js.cx/carousel/6.png"></li>
+//                 <li><img src="https://js.cx/carousel/7.png"></li>
+//                 <li><img src="https://js.cx/carousel/8.png"></li>
+//                 <li><img src="https://js.cx/carousel/9.png"></li>
+//                 <li><img src="https://js.cx/carousel/10.png"></li>
+//             </ul>
+//         </div>
+//         <button class="arrow next">⇨</button>
+//     </div>
+
+
+
+//     <script>
+// этот код помечает картинки цифрами, для удобства разработки
+// его можно убрать, если не нужен
+var lis = document.getElementsByTagName('li');
+for (var i = 0; i < lis.length; i++) {
+    lis[i].style.position = 'relative';
+    var span = document.createElement('span');
+    span.style.cssText = 'position:absolute;left:0;top:0';
+    span.innerHTML = i + 1;
+    lis[i].appendChild(span);
+
+
+}
+// </script>
+
+// <script>
+//це їхній код. розмітку зробив похожу, а от js були проблеми
+var width = 130; // ширина изображения
+var count = 3; // количество изображений
+
+var carousel = document.getElementById('carousel');
+var list = carousel.querySelector('ul');
+var listElems = carousel.querySelectorAll('li');
+
+var position = 0; // текущий сдвиг влево
+
+carousel.querySelector('.prev').onclick = function() {
+    // сдвиг влево
+    // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+    position = Math.min(position + width * count, 0)
+        //тут 
+    list.style.marginLeft = position + 'px';
+};
+
+carousel.querySelector('.next').onclick = function() {
+    // сдвиг вправо
+    // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+    position = Math.max(position - width * count, -width * (listElems.length - count));
+    list.style.marginLeft = position + 'px';
+};
+//     </script>
 // </body>
 
 // </html>
