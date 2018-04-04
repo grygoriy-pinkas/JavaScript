577cf499027bf221711d95c8c6cec448c6e1f0d0
Grisha Pinkas <gricha.p@gmail.com>
2018-04-04 08:52:39 +0200

    n4 to DOM-mouseClick

--- DOM/DOM-mouseClick/1/script.js
+++ DOM/DOM-mouseClick/1/script.js
@@ -8,13 +8,20 @@ function actions(e) {
     let target = e.target;
     if (!e.ctrlKey) //без цієї умови не хотіло ставити виділення з ctrl. чому? 
 
-        target.style.backgroundColor = 'green';
+        target.className = 'highlighted';
     if (e.ctrlKey) {
-
-        target.style.backgroundColor == 'green' ? target.style.backgroundColor = '' : target.style.backgroundColor = 'green';
+        console.log(target.className);
+        if (target.className == 'highlighted') {
+            target.className = ''
+        } else {
+            target.className = 'highlighted'
+        }
+        //target.className = 'highlighted' ? target.className = '' : target.className = 'highlighted';
+        //if(target.className = '')
+        //target.className = '' ? target.className = 'highlighted' : ;
     }
     var lastTarget = target;
-    //console.log(lastTarget);
+
     if (e.shiftKey) {
         //мій перший спосіб - неробочий
         // let finish = target.nextElementSibling
@@ -31,24 +38,25 @@ function actions(e) {
         //     console.log(lastTarget);
         // }
         ifShift(lastTarget, e);
-        //console.log(target);
+
     }
 
 }
 
-function ifShift(target) {
-    //let target = e.target;
+function ifShift(lastTarget, e) {
+    let target = e.target;
     //третій варіант - взятий з розвязку. теж непрацює. допоможи розібратись
     var startElem = lastTarget || ul.children[0];
+    console.log(startElem.compareDocumentPosition(target));
     var isLastClickedBefore = startElem.compareDocumentPosition(target) & 4;
     console.log(lastTarget);
     console.log(startElem);
+    console.log(target);
     console.log(isLastClickedBefore);
     if (isLastClickedBefore) {
         for (var elem = startElem; elem != target; elem = elem.nextElementSibling) {
             console.log("enter");
-            //console.log(target);
-            elem.style.backgroundColor == 'green';
+            target.className = 'highlighted';
         }
     }
 
