6c4f9073f5e575187fdf6cead07a832a36f763df
Grisha Pinkas <gricha.p@gmail.com>
2018-04-07 15:46:15 +0200

    n2 to DOM-keyboardEvents

--- DOM/DOM-keyboardEvents/2/script.js
+++ DOM/DOM-keyboardEvents/2/script.js
@@ -1,40 +1,32 @@
 // Создайте функцию runOnKeys(func, code1, code2, ... code_n), которая запускает func при 
 // одновременном нажатии клавиш со скан-кодами code1, code2, …, code_n.
 
-// document.addEventListener('keypress', )
-// function runOnKeys(func, code1, code2) {
 
-//     console.log(code1);
-//     console.log(code2);
-// }
-
+//після поглянув в розвязок то виправивися трохи)))
 
 function runOnKeys(func, code1, code2, ...code_n) {
     var presed = {};
     var arg = [].slice.call(arguments);
     var func = arg.splice(0, 1)[0];
-    var newArr = [];
     document.onkeydown = function(e) {
-        // console.log(e.keyCode);
-        if (!("e.keyCode" in presed)) {
-            presed[e.keyCode] = true;
-        }
 
-        // var size = Object.keys(myObj).length;
-        //console.log("e.keyCode" in presed);
-        //  console.log(func);
-        console.log(arg.length);
-        if (arg.length == Object.keys(presed).length) {
-            func();
+        // if (!("e.keyCode" in presed)) {
+        presed[e.keyCode] = true;
+        // }
+
+        for (var i = 0; i < arg.length; i++) { // проверить, все ли клавиши нажаты
+            if (!presed[arg[i]]) {
+                return;
+            }
         }
-        console.log(presed.length);
+        presed = {};
+
+        func();
+        console.log(presed);
     }
     document.onkeyup = function(e) {
         delete presed[e.keyCode];
-        // console.log(presed);
     }
-
-
 }
 
 runOnKeys(
@@ -43,16 +35,16 @@ runOnKeys(
     "W".charCodeAt(0)
 );
 
-function getChar(event) {
-    if (event.which == null) {
-        if (event.keyCode < 32) return null;
-        return String.fromCharCode(event.keyCode) // IE
-    }
+// function getChar(event) {
+//     if (event.which == null) {
+//         if (event.keyCode < 32) return null;
+//         return String.fromCharCode(event.keyCode) // IE
+//     }
 
-    if (event.which != 0 && event.charCode != 0) {
-        if (event.which < 32) return null;
-        return String.fromCharCode(event.which) // остальные
-    }
+//     if (event.which != 0 && event.charCode != 0) {
+//         if (event.which < 32) return null;
+//         return String.fromCharCode(event.which) // остальные
+//     }
 
-    return null; // специальная клавиша
-}
+//     return null; // специальная клавиша
+// }
