98c298313f6a0ad8b0d54d82dde4165fd60b81ea
Grygoriy Pinkas <30478760+grygoriy-pinkas@users.noreply.github.com>
2018-04-15 12:51:44 +0200

    n1 to DOM-form (#52)
    * n1 to DOM-form
    
    * n2 to DOM-form

---
+++ DOM/DOM-form/1/script.js
@@ -0,0 +1,35 @@
+// Добавьте опцию к селекту
+// важность: 5
+
+// При помощи JavaScript:
+
+// Выведите значение и текст текущей выбранной опции.
+// Добавьте опцию: <option value="Classic">Классика</option>.
+// Сделайте её выбранной.
+
+// console.log(document.forms);
+
+var form = document.forms.form;
+var select = form.select;
+
+showSelected(select);
+
+var option = new Option("Классика", "Classic", true, true);
+select.appendChild(option);
+
+showSelected(select);
+
+// function showSelected(select) {
+//     for (var i = 0; i < select.options.length; i++) {
+//         var option = select.options[i];
+//         if (option.selected) {
+//             alert(option.value + " " + option.text);
+//         }
+//     }
+// }
+
+function showSelected(select) {
+    var selectedOption = select.options[select.selectedIndex];
+    alert(selectedOption.value + " - " + selectedOption.text);
+
+}
